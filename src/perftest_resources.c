f4ccd29899ecf75ab4f3aa293081595c5101a3e6
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2023-04-21 10:57:58 +0200

    remove_debug

--- src/perftest_resources.c
+++ src/perftest_resources.c
@@ -2747,11 +2747,11 @@ struct ibv_qp* ctx_qp_create(struct pingpong_context *ctx,
 			qp_cap->max_inline_data, user_param->inline_size);*/
 		user_param->inline_size = qp_cap->max_inline_data;
 	}
-	if(qp == NULL){
+	/*if(qp == NULL){
 		printf("QP is null\n");
 	}else{
 		printf("Pointer to the kernel qp : %p\n", qp->k_qp);
-	}	
+	}*/	
 
 	return qp;
 }
